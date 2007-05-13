3fcf595ddfaf85b2afced8c46424eb61ce619470
Torwidev <Torwidev@0ce51f5b-8d2f-0410-9d1e-4b921ef835b4>
2007-05-13 19:45:58 +0000

    *SQL galdera zuzenduta

--- trunk/RMI + DB/zerbitzaria/kudeatzaileak/TxartelIrakKud.java
+++ trunk/RMI + DB/zerbitzaria/kudeatzaileak/TxartelIrakKud.java
@@ -22,12 +22,12 @@ public class TxartelIrakKud {
 
 	public int getHelburuGunea(int ateid,int guneid) 
 			throws IllegalStateException, SQLException {
-		String c6="SELECT T.guneId " +
-				  "FROM txartelirakurgailuak AS T" +
-				  "WHERE T.ateId="+ateid+" AND T.guneId!="+guneid;
+		String c6="SELECT guneId " +
+				  "FROM txartelirakurgailuak " +
+				  "WHERE ateId="+ateid+" AND guneId!="+guneid;
 		ResultSet q = agindua.executeQuery(c6);
 		if (q.next())
-			return q.getInt("T.guneId");
+			return q.getInt("guneId");
 		else
 			return -1;
 					
